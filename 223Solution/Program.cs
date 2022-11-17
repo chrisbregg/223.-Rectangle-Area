ce77134a490e7155c08574eff416bd1faf4e956f
Christopher Bregg <chrisbregg@outlook.com>
2022-11-16 20:16:32 -0800

    Initial Commit

---
+++ 223Solution/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Solution {
+    public static int Main(string[] args)
+    {
+        return 0;
+    }
+
+    public int ComputeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        return 0;
+    }
+}
---
+++ 223Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace _223Test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
