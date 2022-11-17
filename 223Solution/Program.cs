a74aea2f06e265fc6691ba09933d4af117ebe44a
Christopher Bregg <chrisbregg@outlook.com>
2022-11-16 20:46:23 -0800

    Implemented ComputeArea

--- 223Solution/Program.cs
+++ 223Solution/Program.cs
@@ -5,6 +5,43 @@
     }
 
     public int ComputeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        return 0;
+        int aArea = GetArea(ax1, ay1, ax2, ay2);
+        int bArea = GetArea(bx1, by1, bx2, by2);
+        int cArea = 0;
+
+        if (Overlap(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2))
+        {
+            int cx1 = Math.Max(ax1, bx1);
+            int cy1 = Math.Max(ay1, by1);
+            int cx2 = Math.Min(ax2, bx2);
+            int cy2 = Math.Min(ay2, by2);
+
+            cArea = GetArea(cx1, cy1, cx2, cy2);
+        }
+
+        return (aArea + bArea - cArea);
+    }
+
+    private bool Overlap(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2)
+    {
+        if (ax1 > bx2 ||
+            bx1 > ax2 ||
+            ay1 > by2 ||
+            by1 > ay2)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }    
+    }
+
+    private int GetArea(int x1, int y1, int x2, int y2)
+    {
+        int length = x2 - x1;
+        int height = y2 - y1;
+
+        return length * height;
     }
 }
--- 223Test/UnitTest1.cs
+++ 223Test/UnitTest1.cs
@@ -4,7 +4,52 @@ namespace _223Test;
 public class UnitTest1
 {
     [TestMethod]
-    public void TestMethod1()
+    public void NoOverlap()
     {
+        Solution sol = new Solution();
+
+        int area = sol.ComputeArea(0, 0, 10, 10, 10, 10, 20, 20);
+
+        Assert.AreEqual(200, area);
+    }
+
+    [TestMethod]
+    public void TotalOverlap()
+    {
+        Solution sol = new Solution();
+
+        int area = sol.ComputeArea(0, 0, 10, 10, 0, 0, 10, 10);
+
+        Assert.AreEqual(100, area);
+    }
+
+    [TestMethod]
+    public void PartialOverlap()
+    {
+        Solution sol = new Solution();
+
+        int area = sol.ComputeArea(0, 0, 10, 10, 5, 5, 15, 15);
+
+        Assert.AreEqual(175, area);
+    }
+
+    [TestMethod]
+    public void Sample1()
+    {
+        Solution sol = new Solution();
+
+        int area = sol.ComputeArea(-3, 0, 3, 4, 0, -1, 9, 2);
+
+        Assert.AreEqual(45, area);
+    }
+
+    [TestMethod]
+    public void Sample2()
+    {
+        Solution sol = new Solution();
+
+        int area = sol.ComputeArea(-2, -2, 2, 2, -2, -2, 2, 2);
+
+        Assert.AreEqual(16, area);
     }
 }
